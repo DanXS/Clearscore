ef72faed412f5ca5566500c88a2255e6084ec69e
Dan Shepherd <dan.x.shepherd@googlemail.com>
2021-08-10 15:17:44 +0100

    Initial Commit

---
+++ ClearScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClearScore
+//
+//  Created by Dan Shepherd on 10/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
